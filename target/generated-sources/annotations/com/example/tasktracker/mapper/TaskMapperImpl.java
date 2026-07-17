a1c67f3beadbb2687184821b23cf5b7f1fa1947a
GiorgiMM16 <87522328+GiorgiMM16@users.noreply.github.com>
2026-07-17 11:03:12 +0400

    Fix Maven

--- target/generated-sources/annotations/com/example/tasktracker/mapper/TaskMapperImpl.java
+++ target/generated-sources/annotations/com/example/tasktracker/mapper/TaskMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-15T12:32:09+0400",
+    date = "2026-07-17T11:02:57+0400",
     comments = "version: 1.6.3, compiler: javac, environment: Java 26 (Oracle Corporation)"
 )
 @Component
